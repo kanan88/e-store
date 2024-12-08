5af9a89e466d1fee5cd2cf64b32d806aa20d7036
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-08 01:15:43 +0100

    installing shadcn/ui into our project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Button } from '@/components/ui/button';
+
 const App = () => {
-  return <h1 className="text-7xl font-bold">App</h1>;
+  return (
+    <div>
+      <h1 className="text-7xl font-bold">App</h1>
+      <Button
+        variant="destructive"
+        size="lg"
+        onClick={() => console.log('Button clicked...')}
+      >
+        Cick me
+      </Button>
+    </div>
+  );
 };
 
 export default App;
