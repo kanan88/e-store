e7741b9c4a74079b723b69c836ade53387183a5e
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-08 23:01:44 +0100

    dummy about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,20 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+        We love{' '}
+        <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">
+          e-store
+        </span>
+      </h1>
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis
+        magnam dolores nisi nostrum aperiam harum veritatis rerum? Alias maxime
+        obcaecati temporibus natus est omnis, laudantium autem consectetur,
+        assumenda error dicta?
+      </p>
+    </section>
+  );
 };
 
 export default About;
