b3a0a89382ad5a30cf6ef52666d15ece8f0de76a
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-08 01:52:05 +0100

    installing redux-toolkit in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { Button } from '@/components/ui/button';
+import { useAppSelector, useAppDispatch } from './hooks';
 
 const App = () => {
+  const { name } = useAppSelector((state) => state.userState);
+  console.log(name);
+
   return (
     <div>
       <h1 className="text-7xl font-bold">App</h1>
