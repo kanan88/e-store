4787b064158cf784b8279e59a6f39243801ee327
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-08 22:51:46 +0100

    ErrorPage Component

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,14 @@ import {
   Login,
   Checkout,
   Orders,
+  Error,
 } from '@/pages';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomeLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
@@ -50,10 +52,12 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+    errorElement: <Error />,
   },
   {
     path: '/register',
     element: <Register />,
+    errorElement: <Error />,
   },
 ]);
 
