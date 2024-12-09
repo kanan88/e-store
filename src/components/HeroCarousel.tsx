e6c6757395259155835757f84fb7f21d3868569d
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-09 21:40:19 +0100

    hero carousel

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -1,5 +1,43 @@
+import hero1 from '@/assets/hero1.webp';
+import hero2 from '@/assets/hero2.webp';
+import hero3 from '@/assets/hero3.webp';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import { Card, CardContent } from '@/components/ui/card';
+
+const carouselImages = [hero1, hero2, hero3];
+
 const HeroCarousel = () => {
-  return <div>HeroCarousel</div>;
+  return (
+    <div className="hidden lg:block">
+      <Carousel>
+        <CarouselContent>
+          {carouselImages.map((image, index) => {
+            return (
+              <CarouselItem key={index}>
+                <Card>
+                  <CardContent className="p-2">
+                    <img
+                      src={image}
+                      alt="hero"
+                      className="w-full h-[24rem] rounded-md object-cover"
+                    />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 };
 
 export default HeroCarousel;
