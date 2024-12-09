424bdef82e169b642e4b68b848e092bcd51cf73a
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-09 17:46:20 +0100

    loader function with landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,14 @@
 import { FeaturedProducts, Hero } from '@/components';
+import { customFetch, type ProductsResponse } from '@/utils';
+import { type LoaderFunction } from 'react-router-dom';
+
+const url = '/products?featured=true';
+
+export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
+  const response = await customFetch<ProductsResponse>(url);
+
+  return { ...response.data };
+};
 
 const Landing = () => {
   return (
