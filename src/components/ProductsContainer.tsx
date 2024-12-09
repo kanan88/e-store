17c36d8d5602af91cc792b35116b21f2468e5111
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-09 23:41:49 +0100

    products container component

--- src/components/ProductsContainer.tsx
+++ src/components/ProductsContainer.tsx
@@ -1,10 +1,53 @@
-import ProductsList from './ProductsList';
+import { useLoaderData } from 'react-router-dom';
+import ProductsList from '@/components/ProductsList';
+import { type ProductsResponse } from '@/utils';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { LayoutGrid, List } from 'lucide-react';
+import ProductsGrid from './ProductsGrid';
 
 const ProductsContainer = () => {
+  const { meta } = useLoaderData() as ProductsResponse;
+  const totalProducts = meta.pagination.total;
+  const [layout, setLayout] = useState<'grid' | 'list'>('grid');
+
   return (
-    <div>
-      <ProductsList />
-    </div>
+    <>
+      {/* HEADER */}
+      <section>
+        <div className="flex justify-between items-center mt-8">
+          <h4 className="font-medium text-md">
+            {totalProducts} product{totalProducts > 1 && 's'}
+          </h4>
+          <div className="flex gap-x-4">
+            <Button
+              onClick={() => setLayout('grid')}
+              variant={layout === 'grid' ? 'default' : 'ghost'}
+            >
+              <LayoutGrid />
+            </Button>
+            <Button
+              onClick={() => setLayout('list')}
+              variant={layout === 'list' ? 'default' : 'ghost'}
+            >
+              <List />
+            </Button>
+          </div>
+        </div>
+      </section>
+      {/* PRODUCTS */}
+      <div>
+        {totalProducts === 0 ? (
+          <h5 className="text-2xl mt-16">
+            Sorry, no products matched your search...
+          </h5>
+        ) : layout === 'grid' ? (
+          <ProductsGrid />
+        ) : (
+          <ProductsList />
+        )}
+      </div>
+    </>
   );
 };
 
