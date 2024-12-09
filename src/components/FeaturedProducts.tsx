be471d35bab3339d38863fb54aad7ce3106158ac
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-09 20:03:28 +0100

    section title

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -1,5 +1,13 @@
+import ProductsGrid from './ProductsGrid';
+import SectionTitle from './SectionTitle';
+
 const FeaturedProducts = () => {
-  return <div>FeaturedProducts</div>;
+  return (
+    <section className="pt-24">
+      <SectionTitle text="featured products" />
+      <ProductsGrid />
+    </section>
+  );
 };
 
 export default FeaturedProducts;
--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,5 +1,14 @@
-const SectionTitle = () => {
-  return <div>SectionTitle</div>;
+import { Separator } from '@/components/ui/separator';
+
+const SectionTitle = ({ text }: { text: string }) => {
+  return (
+    <div>
+      <h2 className="text-3xl font-medium tracking-wide capitalize mb-8">
+        {text}
+      </h2>
+      <Separator />
+    </div>
+  );
 };
 
 export default SectionTitle;
