8aafe9e354cdb00d4a957a37fec1142c09d7e923
Kanan Farzali <kanan.farzali@gmail.com>
2024-12-11 01:00:05 +0100

    adding pagination functionality

--- src/components/PaginationContainer.tsx
+++ src/components/PaginationContainer.tsx
@@ -1,5 +1,62 @@
+import {
+  constructPreOrNextUrl,
+  constructUrl,
+  type ProductsResponseWithParams,
+} from '@/utils';
+import { useLoaderData, useLocation } from 'react-router-dom';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from './ui/pagination';
+
 const PaginationContainer = () => {
-  return <div>PaginationContainer</div>;
+  const { meta } = useLoaderData() as ProductsResponseWithParams;
+  const { pageCount, page } = meta.pagination;
+
+  const { search, pathname } = useLocation();
+
+  const pages = Array.from({ length: pageCount }, (_, index) => index + 1);
+
+  // Skip pagination if we only have one page
+  if (pageCount < 2) return null;
+
+  const renderPagination = pages.map((pageNumber) => {
+    const isActive = pageNumber === page;
+    const url = constructUrl({ pageNumber, search, pathname });
+
+    return (
+      <PaginationItem key={pageNumber}>
+        <PaginationLink to={url} isActive={isActive}>
+          {pageNumber}
+        </PaginationLink>
+      </PaginationItem>
+    );
+  });
+
+  const { prevUrl, nextUrl } = constructPreOrNextUrl({
+    currentPage: page,
+    pageCount,
+    search,
+    pathname,
+  });
+
+  return (
+    <Pagination className="mt-16">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious to={prevUrl} />
+        </PaginationItem>
+        {renderPagination}
+        <PaginationItem>
+          <PaginationNext to={nextUrl} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
 };
 
 export default PaginationContainer;
---
+++ src/utils/pagination.ts
@@ -0,0 +1,41 @@
+import { current } from '@reduxjs/toolkit';
+
+type ConstructUrlParams = {
+  pageNumber: number;
+  search: string;
+  pathname: string;
+};
+
+export const constructUrl = ({
+  pageNumber,
+  search,
+  pathname,
+}: ConstructUrlParams): string => {
+  const searchParams = new URLSearchParams(search);
+  searchParams.set('page', pageNumber.toString());
+  return `${pathname}?${searchParams.toString()}`;
+};
+
+type ConstructPreOrNextParams = {
+  currentPage: number;
+  pageCount: number;
+  search: string;
+  pathname: string;
+};
+
+export const constructPreOrNextUrl = ({
+  currentPage,
+  pageCount,
+  search,
+  pathname,
+}: ConstructPreOrNextParams): { prevUrl: string; nextUrl: string } => {
+  let prevPage = currentPage - 1;
+  if (prevPage < 1) prevPage = pageCount;
+  const prevUrl = constructUrl({ pageNumber: prevPage, search, pathname });
+
+  let nextPage = currentPage + 1;
+  if (nextPage > pageCount) nextPage = 1;
+  const nextUrl = constructUrl({ pageNumber: nextPage, search, pathname });
+
+  return { prevUrl, nextUrl };
+};
